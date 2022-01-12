6c22ee347adfe799524a1504cb9d9a0eecd061f5
richardjarrell <richardjarrell@yahoo.com>
2022-01-12 08:09:11 -0600

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
-import "./style.css";
+import React, { useState } from 'react';
+import { Grid, Button } from '@material-ui/core';
+import './style.css';
 
 export default function App() {
+  const [page, setPage] = useState();
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <>
+      <Button>Upload</Button>
+      <Button>Default</Button>
+      <Button>Default</Button>
+      <Button>Default</Button>
+      <Button>Default</Button>
+      <Button>Default</Button>
+
+      <Grid>
+        <div>
+          <h1>Hello StackBlitz!</h1>
+          <p>Start editing to see some magic happen :)</p>
+        </div>
+      </Grid>
+    </>
   );
 }
