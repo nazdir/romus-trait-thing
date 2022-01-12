38509ee32aa991aef1111cb2ebb96f23885d002c
richardjarrell <richardjarrell@yahoo.com>
2022-01-12 09:39:52 -0600

    1 file modified

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -1,11 +1,34 @@
 import React from 'react';
-import { Grid, Button, TextField } from '@material-ui/core';
+import { Grid, Button, TextField, MenuItem } from '@material-ui/core';
 
 export const Upload = () => {
   return (
     <>
-      <TextField label="File" />
-      <Button variant="contained">Upload file</Button>
+      <TextField
+        id="standard-select-currency"
+        select
+        label="Select"
+        helperText="select your file upload type"
+        margin="normal"
+      >
+        <MenuItem value="Market Year">Market Year</MenuItem>
+        <MenuItem value="Market Year">Crop</MenuItem>
+        <MenuItem value="Market Year">Zone</MenuItem>
+        <MenuItem value="Market Year">Year/Crop/Price</MenuItem>
+      </TextField>
+      <br />
+      <input
+        accept="image/*"
+        id="contained-button-file"
+        multiple
+        type="file"
+        style={{ display: 'none' }}
+      />
+      <label htmlFor="contained-button-file">
+        <Button variant="contained" component="span">
+          Upload
+        </Button>
+      </label>
     </>
   );
 };
